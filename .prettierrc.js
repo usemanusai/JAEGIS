6ab46760e958f5a4faf5ab4d1b2343a1f05ec562
usemanusai <use.manus.ai@gmail.com>
2025-07-26 03:52:05 +0200

    ⚙️ Create Prettier configuration for code formatting

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  useTabs: false,
+  quoteProps: 'as-needed',
+  bracketSameLine: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  embeddedLanguageFormatting: 'auto'
+}
